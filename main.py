319e000ddcf241e1cec947f86732ae3ed83f5fd4
deexth <hkabare@gmail.com>
2025-11-09 18:27:22 +0100

    bookbot final

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def get_book_text(file_path):
 
 
 def main():
-    if len(sys.argv) < 2 or len(sys.argv) > 2:
+    if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
 
